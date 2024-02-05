437de8be93a86e9c4cca3b3ad2528143d6a4dfba
souvik141998 <souvik.chowdhury@msds.christuniversity.in>
2024-02-05 18:40:56 +0530

    update

--- questions/views.py
+++ questions/views.py
@@ -15,16 +15,38 @@ def createQuestion(request):
     if request.method == "POST":
         quiz = Quiz.objects.get(id=request.POST.get('quiz'))
         questionText = request.POST.get('questionText')
+        answer1=request.POST.get('answer1')
+        answer2=request.POST.get('answer2')
+        answer3=request.POST.get('answer3')
+        answer4=request.POST.get('answer4')
         answer = request.POST.get('answer')
-        print(questionText,answer)
         obj = Question(text=questionText,quiz=quiz)
         obj.save()
-        obj1 = Answer(text= answer,question=obj)
-         
-        obj2 = Answer(text= answer,question=obj)
-        obj3 = Answer(text= answer,question=obj)
-        obj4 = Answer(text= answer,question=obj)
-        obj2.save()
-        obj2.save()
+
+        if answer1 == answer:
+            ans=Answer(text=answer1,correct=True,question=obj)
+            ans.save()
+        else:
+            ans=Answer(text=answer1,correct=False,question=obj)
+            ans.save()
+        if answer2 == answer:
+            ans=Answer(text=answer2,correct=True,question=obj)
+            ans.save()
+        else:
+            ans=Answer(text=answer2,correct=False,question=obj)
+            ans.save()
+        if answer3 == answer:
+            ans=Answer(text=answer3,correct=True,question=obj)
+            ans.save()
+        else:
+            ans=Answer(text=answer3,correct=False,question=obj)
+            ans.save()
+        if answer4 == answer:
+            ans=Answer(text=answer4,correct=True,question=obj)
+            ans.save()
+        else:
+            ans=Answer(text=answer4,correct=False,question=obj)
+            ans.save()
+        print(questionText,answer)
     
     return render(request, 'teachersQuestion.html',{'quizzes':quizzes})
--- quizes/views.py
+++ quizes/views.py
@@ -104,3 +104,14 @@ def createQuiz(request):
         return render(request, '/')
     else:
         return render (request , 'teachers.html')
+    
+
+
+
+class QuizListView(ListView):
+   model=Quiz
+   template_name ='quizes/main.html'
+
+def quiz_view(request,pk):
+    quiz = Quiz.objects.get(pk=pk)
+    return render(request,'quizes/quiz.html',{'obj':quiz})
